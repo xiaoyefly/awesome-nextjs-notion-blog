35ffe76b86032d1641c4a7842ccc18ae28ba967d
xiaoyefly <30384954+xiaoyefly@users.noreply.github.com>
2024-03-23 17:08:43 +0800

    notion地址修改

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'SoulEngine-dcb2e61cc5404bffa0fbe59aa1611b3d',
+  rootNotionPageId: 'SoulEngine-2e5b1b4c5a9b4784a40a4892c975d3c7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -23,7 +23,7 @@ module.exports = {
   // wechatPublicURL: 'https://mp.weixin.qq.com/s?__biz=MzkxMzIyOTI1Mg==&mid=2247486027&idx=1&sn=2840f0b8cfdc982b9095dd18ec9892e5',
   wechatPublicName: '',
   wechatPublicURL: '',
-  notionPublic: 'https://climbing-entree-643.notion.site/SoulEngine-dcb2e61cc5404bffa0fbe59aa1611b3d',
+  notionPublic: 'https://climbing-entree-643.notion.site/2e5b1b4c5a9b4784a40a4892c975d3c7',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
